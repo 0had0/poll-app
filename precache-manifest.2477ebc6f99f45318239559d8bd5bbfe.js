47a634bfc5dcff7ae86f1d1faf1e6015087db351
hadi houssainy <hadihoussainy75@gmail.com>
2020-03-30 02:26:27 +0300

    Updates

---
+++ precache-manifest.2477ebc6f99f45318239559d8bd5bbfe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "235bc1a52984259e679a2203c972c76c",
+    "url": "/poll-app/index.html"
+  },
+  {
+    "revision": "fce448c266f9eaa74545",
+    "url": "/poll-app/static/css/main.1ee1d649.chunk.css"
+  },
+  {
+    "revision": "885cf6be5271b8ccdc57",
+    "url": "/poll-app/static/js/2.9a07390b.chunk.js"
+  },
+  {
+    "revision": "ad61e5a3703d8af976e070e9896b739a",
+    "url": "/poll-app/static/js/2.9a07390b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "819e2edc9285dfe63fa9",
+    "url": "/poll-app/static/js/3.e0167318.chunk.js"
+  },
+  {
+    "revision": "a95063120238e2a3c6b9",
+    "url": "/poll-app/static/js/4.9cb6c3c4.chunk.js"
+  },
+  {
+    "revision": "fce448c266f9eaa74545",
+    "url": "/poll-app/static/js/main.cfc43661.chunk.js"
+  },
+  {
+    "revision": "377ace0b93e769f2b5c1",
+    "url": "/poll-app/static/js/runtime-main.a0c8f70b.js"
+  }
+]);
